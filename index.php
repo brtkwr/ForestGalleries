27a53e26366b7f6a8e9156c219d31605bf67c469
Bharat Kunwar <b.kunwar@gmail.com>
2015-11-19 10:39:01 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<head>
+<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="js/prototype.js" type="text/javascript"></script>
+<script src="js/scriptaculous.js?load=effects,builder" type="text/javascript"></script>
+<script src="js/lightbox.js" type="text/javascript"></script>
+<title>Welcome</title>
+</head>
+
+<body bgcolor="#000000">
+
+<div align="center">
+
+<table border="0" width="800" cellspacing="1" cellpadding="0">
+	<tr>
+		<td bgcolor="#522922">
+		<img border="0" src="images/title.jpg" height="200"></td>
+	</tr>
+	<tr>
+		<td>
+		<h3 align="right"><a href="index.php">home</a> | 
+		<a href="gallery.html">galleries</a> | <a href="contact.html">contact</a></h3>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		<h2>Welcome</h2>
+		</td>
+	</tr>
+		<tr>
+			<td>
+<?php
+include("index.txt");
+?>
+			</td>
+		</tr>
+</table>
+</div>
+
